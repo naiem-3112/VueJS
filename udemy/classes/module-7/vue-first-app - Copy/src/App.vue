cf82b30cd80a1cb86be8f4f8a39dbbd0542c924d
ShahadatPOM <16103112@iubat.edu>
2022-09-26 21:49:30 +0600

    coded

--- udemy/classes/module-7/vue-first-app - Copy/src/App.vue
+++ udemy/classes/module-7/vue-first-app - Copy/src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+      <header>My Friends</header>
+      <ul>
+        <friend-contact
+        name="Jack"
+        phone="01909898977"
+        email="jack@gmail.com"
+        favourite="1"
+        ></friend-contact>
+
+        <friend-contact
+        name="Rij"
+        phone="01345769807"
+        email="rij@gmail.com"
+        favourite="0"
+        ></friend-contact>
+
+      </ul>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+`56-=`
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+* {
+  box-sizing: border-box;
 }
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
 </style>
+
+
+
+
---
+++ udemy/classes/module-7/vue-first-app - Copy/src/components/FriendContact.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <li>
+      <h2>{{ name }} {{ isFavouriteVal === "1" ? '(Favourite)' : '' }}</h2>
+      <button @click="isFavourite">Favourite</button>
+      <button @click="toggleDetails">{{ details ? 'Hide' : 'Show' }} Details</button>
+      <ul v-if="details">
+        <li>Phone: {{ phone }}</li>
+        <li>Email: {{ email }}</li>
+      </ul>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  props:[
+    'name',
+    'phone',
+    'email',
+    'favourite',
+  ],
+  data(){
+    return {
+      details: false,
+      isFavouriteVal: this.favourite,
+      friend:{
+        id: 1,
+        name: 'Nuro',
+        phone: '01900099923',
+        email: 'nuro@gmail.com',
+      }
+    }
+  },
+
+  methods:{
+    toggleDetails(){
+      this.details = !this.details;
+    },
+    isFavourite(){
+      if(this.isFavouriteVal==="1"){
+        this.isFavouriteVal = "0";
+      }else{
+        this.isFavouriteVal = "1"
+      }
+    }
+  }
+};
+</script>
